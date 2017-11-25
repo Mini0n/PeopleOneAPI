c7cfd2d764e721c948d8b8bd66bb93250a401193
Eduardo <eduardo.paramo.l@gmail.com>
2017-11-25 16:34:31 -0600

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :catalogs do
+    resources :areas do
+      resources :items
+    end
+  end
+
+  # resources :areas do
+  #   resources :items
+  # end
+
 end
