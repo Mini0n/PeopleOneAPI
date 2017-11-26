cb2616eb6084f95261e8d3d4889a56d77a1a7a26
Eduardo <eduardo.paramo.l@gmail.com>
2017-11-25 18:06:58 -0600

    Areas and Items controllers coded, Areas and Items APIs working!

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,2 +1,44 @@
 class AreasController < ApplicationController
+  include Response
+
+  before_action :set_catalog
+  before_action :set_catalog_area, only: [:show, :update, :destroy]
+
+  def index
+    json_response(@catalog.areas)
+  end
+
+  def show
+    json_response(@area)
+  end
+
+  def create
+    @catalog.areas.create!(area_params)
+    json_response(@catalog, :created)
+  end
+
+  def update
+    @area.update(area_params)
+    head :no_content
+  end
+
+  def destroy
+    @area.destroy
+    head :no_content
+  end
+
+  private
+
+  def area_params
+    params.permit(:name)
+  end
+
+  def set_catalog
+    @catalog = Catalog.find(params[:catalog_id])
+  end
+
+  def set_catalog_area
+    @area = @catalog.areas.find_by!(id: params[:id]) if @catalog
+  end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,46 @@
 class ItemsController < ApplicationController
+  include Response
+  
+    before_action :set_catalog_area
+    before_action :set_catalog_area_item, only: [:show, :update, :destroy]
+  
+    def index
+      json_response(@area.items)
+    end
+  
+    def show
+      json_response(@item)
+    end
+  
+    def create
+      @area.items.create!(item_params)
+      json_response(@area.items, :created)
+    end
+  
+    def update
+      @item.update(item_params)
+      head :no_content
+    end
+  
+    def destroy
+      @item.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def item_params
+      params.permit(:name)
+    end
+  
+    def set_catalog_area
+      # @catalog = Catalog.areas.find(params[:catalog_id]).find(params[:area_id])
+      @area = Area.find(params[:area_id])
+    end
+  
+    def set_catalog_area_item
+      @item = @area.items.find_by!(id: params[:id]) if @area
+    end
+  
+
 end
